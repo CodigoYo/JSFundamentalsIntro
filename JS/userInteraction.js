497637b3bed050758cfb22a18f966735c7179305
Train-to-Tech <businessIntel2020@gmail.com>
2020-11-17 09:32:28 -0600

    finishing up

--- JS/userInteraction.js
+++ JS/userInteraction.js
@@ -4,22 +4,33 @@ JavaScript provides a way to interact with users within visiting web pages. We w
     Alert
 Have you ever seen a dialog box pop-up with a message when you are browsing a website? This was most likely a JavaScript alert message. The syntax for the alert function is as follows:*/
 
-    alert("Message goes here.");
+    // alert("Message goes here.");  UNCOMMENT THIS LINE AND VIEW PAGE IN BROWSER
 //As you can see, the alert function takes in a string message as an argument. This message will be displayed to a user with an OK button to close the dialog.
 
 /**    Confirm
  If you'd like to confirm that a certain action should be taken before performing it (I'm not a robot etc..). This could be useful for something like emptying your trash folder in you email account. Confirm function is a great way to do so.:
 
 // the following line will show the OK/CANCEL confirm dialog*/
-var confirmed = confirm('Are you sure you want to do XYZ?');
-console.log(confirmed); // will be either true or false
+
+
+// var confirmed = confirm('Are you sure you want to do XYZ?');//UNCOMMENT THIS LINE AND THE CONSOLE BELOW AND VIEW IN BROWSER CONSOLE
+// console.log(confirmed); // will be either true or false
+
+
+
+
 //If the user clicks 'OK', the confirm function call will evaluate to true, and will evaluate to false if the user clicks 'CANCEL'.
 
 /**    Prompt
  The prompt function will display a dialog box that allows a user to type in a response to a question that is asked. The syntax for the prompt */
 
-    var userInput = prompt('Please type something:');
-console.log('The user entered: ' + userInput);
+//     var userInput = prompt('Please type something:');//UNCOMMENT THIS LINE AND THE ONE BELOW TO VIEW IN BROWSER
+// console.log('The user entered: ' + userInput);
+
+
+
+
+
 //The prompt function will give us a string value that contains whatever the user typed, or null if the 'Cancel' button is clicked.
 
 /**
